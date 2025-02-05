d0fcad622c86c66b6100bfc89a8f1beaa4a6fc17
sam <contact@justsam.io>
2025-02-05 19:18:01 +0100

    feat: add multi-head transformer

--- orbital/transformers/attention.py
+++ orbital/transformers/attention.py
@@ -3,12 +3,21 @@ import torch.nn as nn
 
 
 class Attention(nn.Module):
-    def __init__(self, d_model: int, *args, **kwargs):
+    def __init__(self, d_model: int, head_count: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if d_model % head_count != 0:
+            raise ValueError("`head_count` is not divisor of `d_model`")
+        
+        self.d_model = d_model
+        self.head_count = head_count
+
+        self.d_k = d_model // head_count
+        self.d_k_sqrt = self.d_k ** 0.5
 
         self.W_q = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
         self.W_k = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
         self.W_v = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
+        self.W_o = nn.Linear(in_features=d_model, out_features=d_model, bias=False)
 
     def forward(
         self,
@@ -18,28 +27,20 @@ class Attention(nn.Module):
         mask: torch.tensor = None,
     ) -> torch.tensor:
         """Forward hook impl."""
-        q = self.W_q(enc_q)  # [batch_size, ctx_len, d_model]
-        k = self.W_k(enc_k)
-        v = self.W_v(enc_v)
+        q = self.W_q(enc_q).view(enc_q.size(0), enc_q.size(1), self.head_count, self.d_k).transpose(1, 2)  # [batch_size, seq_len, d_model] => [batch_size, seq_len, H, Hd] => [batch_size, H, seq_len, Hd]
+        k = self.W_k(enc_k).view(enc_k.size(0), enc_k.size(1), self.head_count, self.d_k).transpose(1, 2)
+        v = self.W_v(enc_v).view(enc_v.size(0), enc_v.size(1), self.head_count, self.d_k).transpose(1, 2)
 
         # A = softmax( (q * kT) / (sqrt(d_k)) ) * v
-        scores = torch.matmul(q, k.transpose(-2, -1)) / (k.size(1) ** 0.5)
+        scores = torch.matmul(q, k.transpose(-2, -1)) / (self.d_k_sqrt)
         if mask is not None:
             mask = mask.to(scores.device)
             scores = scores.masked_fill(mask == 0, float("-inf"))
 
         attention = torch.softmax(scores, dim=-1)
 
-        # [batch_size, ctx_len, d_model]
-        return torch.matmul(attention, v)
+        out = torch.matmul(attention, v)                        # => [batch_size, H, seq_len, Hd]
+        out = out.transpose(1, 2)                               # => [batch_size, seq_len, H, Hd]
+        out = out.view(out.size(0), out.size(1), self.d_model)  # => [batch_size, seq_len, d_model]
 
-
-class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model: int, head_count: int, *args, **kwargs):
-        super().__(*args, **kwargs)
-        if d_model % head_count != 0:
-            raise ValueError("d_model is not divisible by head_count")
-
-        self.d_model = d_model
-        self.head_count = head_count
-        self.d_k = d_model // head_count  # Dimension of each head
+        return self.W_o(out)
